bfc32b00a74f0713c5e220a2dc96cde596f87a9e
Simon Eisenmann <simon@struktur.de>
2016-08-26 10:37:43 +0200

    Add godocs integration

---
+++ doc.go
@@ -0,0 +1,14 @@
+/*
+Package spreed-turnservicecli is providing sub packages to interact with the
+Spreed.ME TURN service.
+
+It contains the following packages:
+
+The `turnservicecli` package provides a client implementation for the TURN
+service api's endpoints.
+*/
+package spreedturnservicecli
+
+import (
+	_ "github.com/strukturag/spreed-turnservicecli/turnservicecli"
+)
---
+++ turnservicecli/turnservicecli.go
@@ -0,0 +1,5 @@
+/*
+Package turnservicecli provides a client impplementation to a TURN service API
+which provides credentials and geo location endpoints.
+*/
+package turnservicecli
